a31775f71bfe41813dd62bc2dced146a8b546892
Sarah Freitas <sarahjfreitas@gmail.com>
2019-04-09 13:06:21 -0300

    mudanca de bd para postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_09_113033) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "timestamps"
